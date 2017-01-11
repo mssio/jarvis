df0e9a69bf033a160dab8a800472b9e80a8cfd06
Mario Suharja <mariostefanos@gmail.com>
2017-01-11 19:13:15 +0700

    Add handler

--- app/controllers/facebook-botkit.js
+++ app/controllers/facebook-botkit.js
@@ -67,292 +67,90 @@ var controller = Botkit.facebookbot({
 
 var bot = controller.spawn({});
 
-// controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
-//     controller.createWebhookEndpoints(webserver, bot, function() {
-//         console.log('ONLINE!');
-//     });
-// });
+require('./facebook-conversations.js');
 
-controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
-controller.api.thread_settings.get_started('sample_get_started_payload');
-controller.api.thread_settings.menu([
-    {
-        "type":"postback",
-        "title":"Hello",
-        "payload":"hello"
-    },
-    {
-        "type":"postback",
-        "title":"Help",
-        "payload":"help"
-    },
-    {
-      "type":"web_url",
-      "title":"Botkit Docs",
-      "url":"https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
-    },
-]);
+var handler = function (obj) {
+  controller.debug('Message received from FB')
+  var message
+  if (obj.entry) {
+    for (var e = 0; e < obj.entry.length; e++) {
+      for (var m = 0; m < obj.entry[e].messaging.length; m++) {
+        var facebook_message = obj.entry[e].messaging[m]
 
-controller.hears(['quick'], 'message_received', function(bot, message) {
+        console.log(facebook_message)
 
-    bot.reply(message, {
-        text: 'Hey! This message has some quick replies attached.',
-        quick_replies: [
-            {
-                "content_type": "text",
-                "title": "Yes",
-                "payload": "yes",
-            },
-            {
-                "content_type": "text",
-                "title": "No",
-                "payload": "no",
-            }
-        ]
-    });
+        // normal message
+        if (facebook_message.message) {
+          message = {
+            text: facebook_message.message.text,
+            user: facebook_message.sender.id,
+            channel: facebook_message.sender.id,
+            timestamp: facebook_message.timestamp,
+            seq: facebook_message.message.seq,
+            mid: facebook_message.message.mid,
+            attachments: facebook_message.message.attachments
+          }
 
-});
+          // save if user comes from m.me adress or Facebook search
+          // create_user_if_new(facebook_message.sender.id, facebook_message.timestamp)
 
-controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
+          controller.receiveMessage(bot, message)
         }
-    });
-});
+        // When a user clicks on "Send to Messenger"
+        else if (facebook_message.optin ||
+                (facebook_message.postback && facebook_message.postback.payload === 'optin')) {
+          message = {
+            optin: facebook_message.optin,
+            user: facebook_message.sender.id,
+            channel: facebook_message.sender.id,
+            timestamp: facebook_message.timestamp
+          }
 
-controller.hears(['silent push reply'], 'message_received', function(bot, message) {
-    reply_message = {
-        text: "This message will have a push notification on a mobile phone, but no sound notification",
-        notification_type: "SILENT_PUSH"
-    }
-    bot.reply(message, reply_message)
-})
+            // save if user comes from "Send to Messenger"
+          create_user_if_new(facebook_message.sender.id, facebook_message.timestamp)
 
-controller.hears(['no push'], 'message_received', function(bot, message) {
-    reply_message = {
-        text: "This message will not have any push notification on a mobile phone",
-        notification_type: "NO_PUSH"
-    }
-    bot.reply(message, reply_message)
-})
-
-controller.hears(['structured'], 'message_received', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-        convo.ask({
-            attachment: {
-                'type': 'template',
-                'payload': {
-                    'template_type': 'generic',
-                    'elements': [
-                        {
-                            'title': 'Classic White T-Shirt',
-                            'image_url': 'http://petersapparel.parseapp.com/img/item100-thumb.png',
-                            'subtitle': 'Soft white cotton t-shirt is back in style',
-                            'buttons': [
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=100',
-                                    'title': 'View Item'
-                                },
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=100',
-                                    'title': 'Buy Item'
-                                },
-                                {
-                                    'type': 'postback',
-                                    'title': 'Bookmark Item',
-                                    'payload': 'White T-Shirt'
-                                }
-                            ]
-                        },
-                        {
-                            'title': 'Classic Grey T-Shirt',
-                            'image_url': 'http://petersapparel.parseapp.com/img/item101-thumb.png',
-                            'subtitle': 'Soft gray cotton t-shirt is back in style',
-                            'buttons': [
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=101',
-                                    'title': 'View Item'
-                                },
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=101',
-                                    'title': 'Buy Item'
-                                },
-                                {
-                                    'type': 'postback',
-                                    'title': 'Bookmark Item',
-                                    'payload': 'Grey T-Shirt'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }, function(response, convo) {
-            // whoa, I got the postback payload as a response to my convo.ask!
-            convo.next();
-        });
-    });
-});
-
-controller.on('facebook_postback', function(bot, message) {
-    // console.log(bot, message);
-   bot.reply(message, 'Great Choice!!!! (' + message.payload + ')');
-
-});
-
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'message_received', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
+          controller.trigger('facebook_optin', [bot, message])
         }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
+        // clicks on a postback action in an attachment
+        else if (facebook_message.postback) {
+          // trigger BOTH a facebook_postback event
+          // and a normal message received event.
+          // this allows developers to receive postbacks as part of a conversation.
+          message = {
+            payload: facebook_message.postback.payload,
+            user: facebook_message.sender.id,
+            channel: facebook_message.sender.id,
+            timestamp: facebook_message.timestamp
+          }
 
-controller.hears(['what is my name', 'who am i'], 'message_received', function(bot, message) {
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
+          controller.trigger('facebook_postback', [bot, message])
 
-                        convo.next();
+          message = {
+            text: facebook_message.postback.payload,
+            user: facebook_message.sender.id,
+            channel: facebook_message.sender.id,
+            timestamp: facebook_message.timestamp
+          }
 
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
+          controller.receiveMessage(bot, message)
         }
-    });
-});
+        // message delivered callback
+        else if (facebook_message.delivery) {
+          message = {
+            optin: facebook_message.delivery,
+            user: facebook_message.sender.id,
+            channel: facebook_message.sender.id,
+            timestamp: facebook_message.timestamp
+          }
 
-controller.hears(['shutdown'], 'message_received', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
+          controller.trigger('message_delivered', [bot, message])
         }
-        ]);
-    });
-});
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'message_received',
-    function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':|] I am a bot. I have been running for ' + uptime + ' on ' + hostname + '.');
-    });
-
-
-
-controller.on('message_received', function(bot, message) {
-    bot.reply(message, 'Try: `what is my name` or `structured` or `call me captain`');
-    return false;
-});
-
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
+        else {
+          controller.log('Got an unexpected message from Facebook: ', facebook_message)
+        }
+      }
     }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
+  }
 }
 
-exports.controller = controller;
+exports.handler = handler;
